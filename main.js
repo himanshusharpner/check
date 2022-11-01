47b7e814d9ff1d63d88b739420d2387bb43e6604
Himan1999 <cu.16bcs1194@gmail.com[B>
2022-11-01 12:06:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', (e)=>{
+    document.querySelector('.btn').style.color = "white";
+    document.querySelector('.btn').style.background = "black";
+
+});
+
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name = event.target.username.value;
+    const email = event.target.emailId.value;
+    localStorage.setItem('name',name);
+    localStorage.setItem('email',email);
+
+}
